1b814b38655c141fd939fe28da62dd68f969d19c
keesckbakker <c.bakker@gmx.net>
2019-11-10 17:47:43 +0100

    Let use know we're running!

--- run.sh
+++ run.sh
@@ -29,13 +29,13 @@ if [ "$need_build" = true ] ; then
 else
   is_running=$(docker ps | grep $name_$program)
   if [ -z "$is_running" ] ; then
-    echo "is not running"
     need_start=true
   fi
 fi
 
 if [ "$need_start" = true ] ; then
   docker-compose up -d program
+  echo "Container is now up and running."
 else
   echo "No changes found. Container is already running."
 fi

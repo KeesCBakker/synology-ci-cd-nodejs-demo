fbe66459f34d89cc4658bb3881736cacfd8bdd11
keesckbakker <c.bakker@gmx.net>
2019-11-10 17:51:34 +0100

    Echo time.

--- src/app.js
+++ src/app.js
@@ -2,6 +2,6 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send('Hello World! My watch says: ' + new Date().toString()))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))

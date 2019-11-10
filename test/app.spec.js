2ac9e659b65ff72624c83792254721ac12eb81d2
keesckbakker <c.bakker@gmx.net>
2019-11-10 18:56:48 +0100

    Make test work again

--- test/app.spec.js
+++ test/app.spec.js
@@ -6,7 +6,10 @@ describe("Hello World App", () => {
     describe("Tests", () => {
 
         it("should just test", () => {
-            expect("Hello World").to.be.empty;
+            expect("Hello World").to.not.be.empty;
+
+            // uncomment this to make the test fail:
+            // expect("Hello World").to.be.empty;
         });
 
     });
